098faadbc4e4e0ee8c408ec45575bc4223140a10
Jeswin Kumar <jeswinpk@agilehead.com>
2016-04-05 11:38:10 +0530

    Fix type annotations

--- src/isotropy.js
+++ src/isotropy.js
@@ -7,9 +7,9 @@ import type { PluginType } from "isotropy-core";
 import type { IsotropyOptionsType, IsotropyResultType } from "isotropy-core";
 import type { IncomingMessage, ServerResponse, Server } from "isotropy-interfaces/node/http";
 
-type IsotropyFnType = (apps: Object, options: IsotropyOptionsType<IncomingMessage, ServerResponse>) => Promise<IsotropyResultType>;
+type IsotropyFnType = (apps: Array<Object>, options: IsotropyOptionsType<IncomingMessage, ServerResponse>) => Promise<IsotropyResultType>;
 
-export default async function(apps: Object, plugins: Array<PluginType>, options: IsotropyOptionsType) : Promise<IsotropyResultType> {
+export default async function(apps: Array<Object>, plugins: Array<PluginType>, options: IsotropyOptionsType) : Promise<IsotropyResultType> {
   const isotropy: IsotropyFnType = getIsotropy(plugins);
 
   options.handler = (router: Router) => (req: IncomingMessage, res: ServerResponse) => {
